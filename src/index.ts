85b178f0f5c605d764d0f2f7274dfea0ad93b4bb
prezzemolo <git@prezzemolo.ga>
2017-03-28 06:09:40 +0900

    index: add niconico video identitifier validator.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import thumbClient from "./clients/thumb";
-import infoClient from "./clients/info";
+import thumbClient from "./clients/thumb"
+// import videoInfoClient from "./clients/video-info"
 
 type Sorriso = {
     category: String,
@@ -28,7 +28,10 @@ type Sorriso = {
 }
 
 export default async (videoId: string): Promise<Sorriso> => {
-    /**
-     * TODO: write core program
-     */
+    /* validate niconico videoId*/
+    const videoIdReg = /^(sm|so|nm|)([0-9]+)$/
+    if (! videoIdReg.test(videoId)) throw new Error("not a valid niconico video identifier.")
+
+    /* WIP */
+    return null
 }
